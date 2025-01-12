87ad7508d38d4b68a50badd3318a7526c170f14c
Manuel Alejandro Pinzón Aguirre <clasefisica2019@gmail.com>
2025-01-12 14:11:43 -0500

    Cuento 1. Inglés: Listo. Inglés-Ortografía: Listo. Español: Listo. Español-Ortografía: Listo. Titulo: No. FormatoIndividual: No. FormatoGeneral: No.

---
+++ 4.ortografia-office/4.Cuento4-office.js
@@ -0,0 +1,94 @@
+//! Sí corregido
+const cuento2 = `
+Un Dólar y ochenta y siente centavos. Eso era todo. Y sesenta centavos de eso eran en monedas de un centavo. Monedas ahorradas de una y dos a la vez al presionar al tendero, al vendedor de verduras y al carnicero, hasta que la mejilla de uno ardía con la silenciosa imputación de parsimonia que implicaba tal trato cercano. Della lo contó tres veces. Un dólar y ochenta y siete centavos. Y al día siguiente sería Navidad.
+
+No quedaba nada por hacer más que dejarse caer en el pequeño y desgastado sofá y llorar. Así que Della lo hizo. Lo que provoca la reflexión moral de que la vida se compone de sollozos, suspiros y sonrisas, predominando los suspiros.
+
+Mientras la dueña de la casa pasa gradualmente de la primera etapa a la segunda, echemos un vistazo a la casa. Un apartamento amueblado a $8 por semana. No era exactamente indescriptible, pero ciertamente tenía esa palabra en busca del escuadrón de la mendicidad.
+
+En el vestíbulo de abajo había un buzón en el que no cabía ninguna carta, y un timbre eléctrico del que ningún dedo mortal podía sacar un sonido. También había una tarjeta con el nombre 'Sr. James Dillingham Young'.
+
+El 'Dillingham' había sido lanzado al viento durante un período anterior de prosperidad cuando su poseedor ganaba $30 por semana. Ahora, cuando el ingreso se había reducido a $20, las letras de 'Dillingham' parecían borrosas, como si estuvieran pensando seriamente en contraerse a una modesta y discreta D. Pero cada vez que el Sr. James Dillingham Young llegaba a casa y subía a su apartamento, era llamado 'Jim' y abrazado con entusiasmo por la Sra. James Dillingham Young, ya presentada como Della. Lo cual está muy bien.
+
+Della terminó de llorar y se arregló las mejillas con el paño de polvo. Se paró junto a la ventana y miró con indiferencia a un gato gris caminando por una cerca gris en un patio trasero gris. Mañana sería Navidad, y solo tenía $1.87 para comprarle un regalo a Jim. Había estado ahorrando cada centavo que podía durante meses, con este resultado. Veinte dólares a la semana no alcanzan mucho. Los gastos habían sido mayores de lo que había calculado. Siempre lo son. Solo $1.87 para comprar un regalo para Jim. Su Jim. Muchas horas felices había pasado planeando algo bonito para él. Algo fino y raro y valioso, algo que estuviera un poco cerca de ser digno del honor de ser poseído por Jim.
+
+Había un espejo entre las ventanas de la habitación. Quizás hayas visto un espejo en un apartamento de $8. Una persona muy delgada y ágil puede, al observar su reflejo en una rápida secuencia de tiras longitudinales, obtener una concepción bastante precisa de su apariencia. Della, siendo delgada, había dominado el arte.
+
+De repente, se apartó de la ventana y se paró frente al espejo. Sus ojos brillaban intensamente, pero su rostro había perdido su color en veinte segundos. Rápidamente se soltó el cabello y lo dejó caer a su longitud completa.
+
+Ahora, había dos posesiones de los James Dillingham Young en las que ambos se enorgullecían enormemente. Una era el reloj de oro de Jim, que había sido de su padre y de su abuelo. La otra era el cabello de Della. Si la Reina de Saba hubiera vivido en el apartamento al otro lado del patio, Della habría dejado su cabello colgando por la ventana algún día para secarse, solo para depreciar las joyas y regalos de Su Majestad. Si el Rey Salomón hubiera sido el conserje, con todos sus tesoros amontonados en el sótano, Jim habría sacado su reloj cada vez que pasara, solo para verlo tirar de su barba por envidia.
+
+Así que ahora el hermoso cabello de Della caía a su alrededor, ondulante y brillante como una cascada de aguas marrones. Llegaba más allá de su rodilla y casi se convertía en una prenda para ella. Y luego lo recogió de nuevo, nerviosa y rápidamente. Una vez vaciló por un minuto y se quedó quieta mientras una o dos lágrimas caían sobre la alfombra roja desgastada.
+
+Se puso su vieja chaqueta marrón; se puso su viejo sombrero marrón. Con un giro de faldas y con el brillante destello aún en sus ojos, salió por la puerta y bajó las escaleras hasta la calle.
+
+Donde se detuvo, el letrero decía: 'Mme. Sofronie. Artículos de Cabello de Todo Tipo.' Subió un piso corriendo, y se recompuso, jadeando. Madame, grande, demasiado blanca, fría, apenas parecía la 'Sofronie'.
+
+'¿Comprará mi cabello?' preguntó Della.
+
+'Compro cabello', dijo Madame. 'Quítate el sombrero y déjame ver cómo se ve.'
+
+La cascada marrón se deslizó hacia abajo.
+
+'Veinte dólares', dijo Madame, levantando la masa con una mano experta.
+
+'Dámelo rápido', dijo Della.
+
+Oh, y las siguientes dos horas pasaron con alas rosadas. Olvida la metáfora trillada. Estaba buscando en las tiendas el regalo para Jim.
+
+Finalmente lo encontró. Seguramente había sido hecho para Jim y para nadie más. No había otro igual en ninguna de las tiendas, y las había recorrido todas. Era una cadena de reloj de platino, simple y sobria en diseño, proclamando adecuadamente su valor solo por su sustancia y no por una ornamentación meretricia, como deben hacer todas las cosas buenas. Era incluso digna del Reloj. Tan pronto como la vio, supo que debía ser de Jim. Era como él. Tranquilidad y valor: la descripción se aplicaba a ambos. Le quitaron veintiún dólares por ella, y se apresuró a casa con los 87 centavos.
+
+Con esa cadena en su reloj, Jim podría estar adecuadamente ansioso por la hora en cualquier compañía. Por grandioso que fuera el reloj, a veces lo miraba a escondidas debido a la vieja correa de cuero que usaba en lugar de una cadena.
+
+Cuando Della llegó a casa, su embriaguez dio paso un poco a la prudencia y la razón. Sacó sus tenacillas, encendió el gas y se puso a trabajar reparando los estragos causados por la generosidad añadida al amor. Lo cual siempre es una tarea tremenda, queridos amigos, una tarea gigantesca.
+
+En cuarenta minutos su cabeza estaba cubierta de pequeños rizos pegados que la hacían parecer maravillosamente como un niño escolar fugitivo. Se miró en el espejo largo, cuidadosamente y críticamente.
+
+'Si Jim no me mata', se dijo a sí misma, 'antes de mirarme por segunda vez, dirá que parezco una corista de Coney Island. Pero, ¿qué podía hacer? ¡Oh! ¿Qué podía hacer con un dólar y ochenta y siete centavos?'
+
+A las siete en punto el café estaba hecho y la sartén estaba en la parte trasera de la estufa, caliente y lista para cocinar las chuletas.
+
+Jim nunca llegaba tarde. Della dobló la cadena del reloj en su mano y se sentó en la esquina de la mesa cerca de la puerta por la que siempre entraba. Luego escuchó sus pasos en las escaleras, muy abajo en el primer tramo, y se puso pálida por un momento. Tenía la costumbre de decir pequeñas oraciones silenciosas sobre las cosas más simples de cada día, y ahora susurró: 'Por favor, Dios, haz que piense que todavía soy bonita.'
+
+La puerta se abrió y Jim entró y la cerró. Parecía delgado y muy serio. Pobre chico, solo tenía veintidós años, ¡y estar cargado con una familia! Necesitaba un abrigo nuevo y no tenía guantes.
+
+Jim entró por la puerta, tan inmóvil como un perro de caza al olor de la codorniz. Sus ojos estaban fijos en Della, y había una expresión en ellos que ella no podía leer, y eso la aterrorizó. No era ira, sorpresa, desaprobación, horror, ni ninguno de los sentimientos para los que se había preparado. Simplemente la miraba fijamente con esa peculiar expresión en su rostro.
+
+Della se deslizó de la mesa y fue hacia él.
+
+'Jim, cariño', gritó, 'no me mires así. Me corté el cabello y lo vendí porque no podría haber pasado la Navidad sin darte un regalo. Volverá a crecer, no te importará, ¿verdad? Tenía que hacerlo. Mi cabello crece rápido. Di "¡Feliz Navidad!" Jim, y seamos felices. No sabes qué bonito, qué hermoso regalo tengo para ti.'
+
+'¿Te cortaste el cabello?' preguntó Jim, laboriosamente, como si aún no hubiera llegado a ese hecho evidente incluso después del mayor esfuerzo mental.
+
+'Lo corté y lo vendí', dijo Della. '¿No te gusto igual, de todos modos? Soy yo sin mi cabello, ¿no?'
+
+Jim miró alrededor de la habitación con curiosidad.
+
+'¿Dices que tu cabello se ha ido?' dijo con un aire casi de idiotez.
+
+'No necesitas buscarlo', dijo Della. 'Te digo que está vendido, vendido y se ha ido también. Es Nochebuena, chico. Sé bueno conmigo, porque se fue por ti. Tal vez los cabellos de mi cabeza estaban contados', continuó con una repentina dulzura seria, 'pero nadie podría contar mi amor por ti. ¿Pongo las chuletas, Jim?'
+
+Jim pareció despertar rápidamente de su trance. Abrazó a su Della. Durante diez segundos, consideremos con discreta atención algún objeto inconsecuente en la otra dirección. Ocho dólares a la semana o un millón al año, ¿cuál es la diferencia? Un matemático o un ingenioso te daría la respuesta incorrecta. Los magos trajeron regalos valiosos, pero eso no estaba entre ellos. Esta oscura afirmación se iluminará más adelante.
+
+Jim sacó un paquete de su bolsillo del abrigo y lo arrojó sobre la mesa.
+
+'No te equivoques, Dell', dijo, 'acerca de mí. No creo que haya nada en el camino de un corte de cabello, un afeitado o un champú que pueda hacer que me guste menos mi chica. Pero si desenvuelves ese paquete, puedes ver por qué me tenías preocupado un rato al principio.'
+
+Dedos blancos y ágiles rasgaron la cuerda y el papel. Y luego un grito extático de alegría; y luego, ¡ay! Un rápido cambio femenino a lágrimas histéricas y lamentos, que requirieron el empleo inmediato de todos los poderes reconfortantes del señor del apartamento.
+
+Porque allí estaban Los Peines, el juego de peines, lateral y trasero, que Della había adorado durante mucho tiempo en una ventana de Broadway. Hermosos peines, de carey puro, con bordes de joyas, justo el tono para usar en el hermoso cabello desaparecido. Eran peines caros, lo sabía, y su corazón simplemente los había deseado y anhelado sin la menor esperanza de poseerlos. Y ahora eran suyos, pero los mechones que deberían haber adornado los codiciados adornos se habían ido.
+
+Pero los abrazó contra su pecho, y finalmente pudo mirar hacia arriba con ojos empañados y una sonrisa y decir: '¡Mi cabello crece tan rápido, Jim!'
+
+Y luego Della saltó como un pequeño gato chamuscado y gritó: '¡Oh, oh!'
+
+Jim aún no había visto su hermoso regalo. Ella se lo tendió ansiosamente en su palma abierta. El metal precioso y opaco parecía brillar con un reflejo de su brillante y ardiente espíritu.
+
+'¿No es una maravilla, Jim? Busqué por toda la ciudad para encontrarlo. Tendrás que mirar la hora cien veces al día ahora. Dame tu reloj. Quiero ver cómo se ve en él.'
+
+En lugar de obedecer, Jim se dejó caer en el sofá y puso sus manos detrás de su cabeza y sonrió.
+
+'Dell', dijo, 'guardemos nuestros regalos de Navidad y mantengámoslos un tiempo. Son demasiado bonitos para usarlos ahora mismo. Vendí el reloj para conseguir el dinero para comprar tus peines. Y ahora supongo que puedes poner las chuletas.'
+
+Los magos, como sabes, eran hombres sabios, maravillosamente sabios, que trajeron regalos al Niño en el pesebre. Inventaron el arte de dar regalos de Navidad. Siendo sabios, sus regalos sin duda eran sabios, posiblemente con el privilegio de intercambio en caso de duplicación. Y aquí te he relatado torpemente la crónica intrascendente de dos niños tontos en un apartamento que imprudentemente sacrificaron el uno por el otro los mayores tesoros de su casa. Pero en una última palabra para los sabios de estos días, que se diga que de todos los que dan regalos, estos dos fueron los más sabios. De todos los que dan y reciben regalos, como ellos, son los más sabios. En todas partes son los más sabios. Ellos son los magos.
+`;
