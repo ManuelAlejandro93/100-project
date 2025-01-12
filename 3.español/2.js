cab8df813bcf8647d901cbe510a0c3849591a3d8
Manuel Alejandro Pinzón Aguirre <clasefisica2019@gmail.com>
2025-01-12 12:14:34 -0500

    Cuento 2. Inglés: Listo. Inglés-Ortografía: Listo. Español: Listo. Español-Ortografía: No. Titulo: No. FormatoIndividual: No. FormatoGeneral: No.

---
+++ 3.español/2.js
@@ -0,0 +1,65 @@
+const cuento2 = `
+A la medianoche, el café estaba lleno. Por alguna casualidad, la pequeña mesa en la que me senté había escapado a la vista de los recién llegados, y dos sillas vacías en ella extendían sus brazos con hospitalidad venal al influjo de clientes.
+
+Y entonces un cosmopolita se sentó en una de ellas, y me alegré, porque sostenía la teoría de que desde Adán no ha existido un verdadero ciudadano del mundo. Oímos hablar de ellos, y vemos etiquetas extranjeras en mucho equipaje, pero encontramos viajeros en lugar de cosmopolitas.
+
+Invoco su consideración de la escena: las mesas con tapas de mármol, la fila de asientos tapizados en cuero en la pared, la alegre compañía, las damas vestidas en trajes de semi-gala, hablando en un exquisito coro visible de gusto, economía, opulencia o arte, los garçons diligentes y amantes de la generosidad, la música sabiamente atendiendo a todos con sus incursiones en los compositores; la mezcla de charla y risas, y, si lo desea, el Würzburger en los altos conos de vidrio que se inclinan hacia sus labios como una cereza madura se balancea en su rama hacia el pico de un arrendajo ladrón. Un escultor de Mauch Chunk me dijo que la escena era verdaderamente parisina.
+
+Mi cosmopolita se llamaba E. Rushmore Coglan, y se le escuchará el próximo verano en Coney Island. Va a establecer una nueva 'atracción' allí, me informó, ofreciendo diversión real.
+
+Y luego su conversación resonó a lo largo de paralelos de latitud y longitud. Tomó el gran mundo redondo en su mano, por así decirlo, familiarmente, con desdén, y no parecía más grande que la semilla de una cereza Maraschino en un pomelo de menú del día. Habló irrespetuosamente del ecuador, saltó de continente en continente, se burló de las zonas y limpió los mares altos con su servilleta. Con un gesto de su mano, hablaba de cierto bazar en Hyderabad. ¡Fiu! Te tendría en esquís en Laponia. ¡Zas! Ahora montabas las olas con los Kanakas en Kealaikahiki. ¡Presto! Te arrastraba a través de un pantano de robles post en Arkansas, te dejaba secar por un momento en las llanuras de álcali de su rancho en Idaho y luego te lanzaba a la sociedad de archiduques vieneses. Anon te contaría sobre un resfriado que adquirió en una brisa del lago de Chicago y cómo el viejo Escamila lo curó en Buenos Aires con una infusión caliente de la hierba chuchula. Habrías dirigido la carta a 'E. Rushmore Coglan, Esq., la Tierra, Sistema Solar, el Universo', y la habrías enviado por correo, sintiéndote seguro de que le sería entregada.
+
+Estaba seguro de que finalmente había encontrado al único verdadero cosmopolita desde Adán, y escuché su discurso mundial con temor de descubrir en él la nota local del mero trotamundos. Pero sus opiniones nunca vacilaron ni decayeron; era tan imparcial con las ciudades, países y continentes como los vientos o la gravedad.
+
+Y mientras E. Rushmore Coglan parloteaba sobre este pequeño planeta, pensé con el júbilo de un gran casi-cosmopolita que escribió para todo el mundo y se dedicó a Bombay. En un poema tiene que decir que hay orgullo y rivalidad entre las ciudades de la tierra, y que 'los hombres que se crían de ellas, trafican arriba y abajo, pero se aferran al dobladillo de sus ciudades como un niño al vestido de su madre'. Y siempre que caminan 'por calles rugientes desconocidas' recuerdan su ciudad natal 'más fiel, tonta, cariñosa; haciendo de su mero nombre respirado su vínculo sobre su vínculo'. Y mi júbilo se despertó porque había atrapado al Sr. Kipling durmiendo. Aquí había encontrado a un hombre no hecho de polvo; uno que no tenía alardes estrechos de lugar de nacimiento o país, uno que, si alardeaba de algo, alardearía de su globo entero contra los marcianos y los habitantes de la Luna.
+
+La expresión sobre estos temas fue precipitada de E. Rushmore Coglan por la necesidad del tercer rincón de nuestra mesa. Mientras Coglan me describía la topografía a lo largo del Ferrocarril Siberiano, la orquesta se deslizó en un popurrí. El aire concluyente fue 'Dixie', y mientras las notas estimulantes salían a borbotones, casi fueron dominadas por un gran aplauso de casi todas las mesas.
+
+Vale la pena un párrafo decir que esta notable escena se puede presenciar cada noche en numerosos cafés de la Ciudad de Nueva York. Se han consumido toneladas de cerveza sobre teorías para explicarlo. Algunos han conjeturado apresuradamente que todos los sureños en la ciudad se dirigen a los cafés al anochecer. Este aplauso del aire 'rebelde' en una ciudad del Norte me desconcierta un poco, pero no es irresoluble. La guerra con España, muchos años de generosos cultivos de menta y sandía, algunos ganadores de apuestas largas en la pista de carreras de Nueva Orleans, y los brillantes banquetes dados por los ciudadanos de Indiana y Kansas que componen la Sociedad de Carolina del Norte, han hecho del Sur una especie de 'moda' en Manhattan. Tu manicura susurrará suavemente, y tu dedo índice izquierdo le recuerda tanto a un caballero en Richmond, VA. Oh, ciertamente; pero muchas damas tienen que trabajar ahora - la guerra, ya sabes.
+
+Cuando se tocaba 'Dixie', un joven de cabello oscuro saltó de algún lugar con un grito de guerrilla de Mosby y agitó frenéticamente su sombrero de ala blanda. Luego se perdió entre el humo, se dejó caer en la silla vacía de nuestra mesa y sacó cigarrillos.
+
+La noche era el período en que la reserva se descongelaba. Uno de nosotros mencionó tres Würzburgers al camarero; el joven de cabello oscuro reconoció su inclusión en el pedido con una sonrisa y un asentimiento. Me apresuré a hacerle una pregunta porque quería probar una teoría que tenía.
+
+'¿Te importaría decirme,' comencé, 'si eres de - '
+
+El puño de E. Rushmore Coglan golpeó la mesa y me quedé en silencio.
+
+'Discúlpame,' dijo él, 'pero esa es una pregunta que nunca me gusta escuchar. ¿Qué importa de dónde es un hombre? ¿Es justo juzgar a un hombre por su dirección postal? He visto a kentuckianos que odiaban el whisky, a virginianos que no descendían de Pocahontas, a indianos que no habían escrito una novela, a mexicanos que no usaban pantalones de terciopelo con dólares de plata cosidos a lo largo de las costuras, a ingleses graciosos, a yanquis derrochadores, a sureños de sangre fría, a occidentales de mente estrecha y a neoyorquinos que estaban demasiado ocupados para detenerse una hora en la calle para ver a un empleado de tienda de comestibles con un solo brazo envolver arándanos en bolsas de papel. Deja que un hombre sea un hombre y no lo etiquetes con la etiqueta de ninguna sección.'
+
+'Perdóname,' dije, 'pero mi curiosidad no era del todo ociosa. Conozco el Sur, y cuando la banda toca "Dixie" me gusta observar. He formado la creencia de que el hombre que aplaude ese aire con especial violencia y lealtad ostensiblemente seccional es invariablemente un nativo de Secaucus, N.J. o del distrito entre Murray Hill Lyceum y el río Harlem, esta ciudad. Estaba a punto de poner mi opinión a prueba preguntando a este caballero cuando interrumpiste con tu propia - teoría más amplia, debo confesar.'
+
+Y ahora el joven de cabello oscuro me habló, y se hizo evidente que su mente también se movía a lo largo de sus propios surcos.
+
+'Me gustaría ser un bígaro,' dijo él, misteriosamente, 'en la cima de un valle, y cantar too-ralloo-ralloo.'
+
+Esto era demasiado oscuro, así que volví a dirigirme a Coglan.
+
+'He estado alrededor del mundo doce veces,' dijo él. 'Conozco a un esquimal en Upernavik que envía a Cincinnati por sus corbatas, y vi a un pastor de cabras en Uruguay que ganó un premio en una competencia de rompecabezas de alimentos para el desayuno de Battle Creek. Pago alquiler de una habitación en El Cairo, Egipto, y otra en Yokohama todo el año. Tengo pantuflas esperándome en una casa de té en Shanghái, y no tengo que decirles cómo cocinar mis huevos en Río de Janeiro o Seattle. Es un mundo muy pequeño. ¿De qué sirve alardear de ser del Norte, o del Sur, o de la vieja casa solariega en el valle, o de Euclid Avenue, Cleveland, o de Pike's Peak, o del condado de Fairfax, Va., o de Hooligan's Flats o de cualquier lugar? Será un mundo mejor cuando dejemos de ser tontos por alguna ciudad mohosa o diez acres de pantano solo porque nacimos allí.'
+
+'Pareces ser un verdadero cosmopolita,' dije admirado. 'Pero también parece que despreciarías el patriotismo.'
+
+'Un vestigio de la Edad de Piedra,' declaró Coglan calurosamente. 'Todos somos hermanos - chinos, ingleses, zulúes, patagones y la gente en la curva del río Kaw. Algún día todo este orgullo mezquino en la ciudad o sección del estado o país de uno será borrado, y todos seremos ciudadanos del mundo, como deberíamos ser.'
+
+'Pero mientras andas por tierras extranjeras,' persistí, '¿no vuelven tus pensamientos a algún lugar - algún querido y - '
+
+'Ningún lugar,' interrumpió E. R. Coglan con ligereza. 'El trozo de materia terrestre, globular, planetario, ligeramente achatado en los polos, y conocido como la Tierra, es mi morada. He conocido a muchos ciudadanos atados a objetos de este país en el extranjero. He visto a hombres de Chicago sentarse en una góndola en Venecia en una noche de luna y alardear de su canal de drenaje. He visto a un sureño al ser presentado al Rey de Inglaterra darle a ese monarca, sin pestañear, la información de que su tía abuela por el lado de su madre estaba relacionada por matrimonio con los Perkinses, de Charleston. Conocí a un neoyorquino que fue secuestrado para pedir rescate por algunos bandidos afganos. Su gente envió el dinero y él regresó a Kabul con el agente. "¿Afganistán?" dijeron los nativos a través de un intérprete. "Bueno, no tan lento, ¿no crees?" "Oh, no sé," dice él, y comienza a contarles sobre un conductor de taxi en la Sexta Avenida y Broadway. Esas ideas no me convienen. No estoy atado a nada que no tenga 8,000 millas de diámetro. Solo ponme como E. Rushmore Coglan, un ciudadano de la esfera terrestre.'
+
+Mi cosmopolita hizo una gran despedida y me dejó, porque pensó que vio a alguien a través del parloteo y el humo a quien conocía. Así que me quedé con el aspirante a bígaro, que fue reducido a Würzburger sin más capacidad para expresar sus aspiraciones de posarse, melodioso, en la cima de un valle.
+
+Me senté reflexionando sobre mi evidente cosmopolita y preguntándome cómo el poeta había logrado pasarlo por alto. Era mi descubrimiento y creía en él. ¿Cómo fue? 'Los hombres que se crían de ellos trafican arriba y abajo pero se aferran al dobladillo de sus ciudades como un niño al vestido de su madre.'
+
+No así E. Rushmore Coglan. Con el mundo entero para su -
+
+Mis meditaciones fueron interrumpidas por un tremendo ruido y conflicto en otra parte del café. Vi por encima de las cabezas de los clientes sentados a E. Rushmore Coglan y a un extraño para mí involucrados en una batalla terrible. Lucharon entre las mesas como titanes, y los vasos se rompieron, y los hombres recogieron sus sombreros y fueron derribados, y una morena gritó, y una rubia comenzó a cantar 'Teasing.'
+
+Mi cosmopolita estaba sosteniendo el orgullo y la reputación de la Tierra cuando los camareros cerraron sobre ambos combatientes con su famosa formación de cuña voladora y los llevaron afuera, todavía resistiendo.
+
+Llamé a McCarthy, uno de los garçons franceses, y le pregunté la causa del conflicto.
+
+'El hombre con la corbata roja' (ese era mi cosmopolita), dijo él, 'se calentó por cosas dichas sobre las aceras malas y el suministro de agua del lugar de donde viene por el otro tipo.'
+
+'¿Por qué,' dije, desconcertado, 'ese hombre es un ciudadano del mundo - un cosmopolita. Él - '
+
+'Originalmente de Mattawamkeag, Maine, dijo,' continuó McCarthy, 'y no soportaría que se hablara mal del lugar.'
+`;
